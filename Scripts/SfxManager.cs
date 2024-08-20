671656a8843ada5264e1abbaed6b2d266172a78f
RedIN3rds <68496400+RedIN3rds@users.noreply.github.com>
2024-08-19 22:54:43 -0400

    SFX System

---
+++ Scripts/SfxManager.cs
@@ -0,0 +1,130 @@
+using Godot;
+using System;
+
+public partial class SfxManager : Node
+{
+    public static SfxManager Instance;
+
+    [Export]
+    public AudioStream[] ChefStreams;
+
+    [Export]
+    public AudioStream[] RobotStreams;
+
+    [Export]
+    public AudioStream[] GeneralStreams;
+
+    [Export]
+    public AudioStreamPlayer ChefPlayer;
+
+    [Export]
+    public AudioStreamPlayer RobotPlayer;
+
+    [Export]
+    public AudioStreamPlayer GeneralPlayer;
+
+    [Export]
+    public float MinVolume = -80f;
+
+    [Export]
+    public float MaxVolume = 6f;
+
+    public override void _Ready()
+    {
+        if (Instance != null) {
+            QueueFree();
+            return;
+        }
+        Instance = this;
+    }
+    public void PlayChefWalking() 
+    {
+        ChefPlayer.Stop();
+        ChefPlayer.Stream = ChefStreams[0];
+        ChefPlayer.Play();
+    }
+    public void PlayChefLadder()
+    {
+        ChefPlayer.Stop();
+        ChefPlayer.Stream = ChefStreams[1];
+        ChefPlayer.Play();
+    }
+    public void PlayChefStirring()
+    {
+        ChefPlayer.Stop();
+        ChefPlayer.Stream = ChefStreams[2];
+        ChefPlayer.Play();
+    }
+    public void PlayChefChopping()
+    {
+        ChefPlayer.Stop();
+        ChefPlayer.Stream = ChefStreams[3];
+        ChefPlayer.Play();
+    }
+    public void PlayRobotWheel() 
+    {
+        RobotPlayer.Stop();
+        RobotPlayer.Stream = RobotStreams[0];
+        RobotPlayer.Play();
+    }
+    public void PlayRobotGrab()
+    {
+        RobotPlayer.Stop();
+        RobotPlayer.Stream = RobotStreams[1];
+        RobotPlayer.Play();
+    }
+    public void PlayShattering() 
+    {
+        GeneralPlayer.Stop();
+        GeneralPlayer.Stream = GeneralStreams[0];
+        GeneralPlayer.Play();
+    }
+    public void PlayBubbling()
+    {
+        GeneralPlayer.Stop();
+        GeneralPlayer.Stream = GeneralStreams[1];
+        GeneralPlayer.Play();
+    }
+    public void SetSFXVolume(float volume) 
+    {
+        float NormalizedVolume = volume * (MaxVolume - MinVolume) + MinVolume;
+        AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex("SFX"), NormalizedVolume);
+    }
+    public void StopChefSFX() 
+    {
+        ChefPlayer.Stop();
+    }
+    public void StopRobotSFX()
+    {
+        RobotPlayer.Stop();
+    }
+    public void StopGeneralSFX()
+    {
+        ChefPlayer.Stop();
+    }
+
+    public void StartChefSFX()
+    {
+        ChefPlayer.Play();
+    }
+    public void StartRobotSFX()
+    {
+        RobotPlayer.Play();
+    }
+    public void StartGeneralSFX()
+    {
+        ChefPlayer.Play();
+    }
+    public void StopAll() 
+    {
+        ChefPlayer.Stop();
+        RobotPlayer.Stop();
+        GeneralPlayer.Stop();
+    }
+    public void StartAll()
+    {
+        ChefPlayer.Play();
+        RobotPlayer.Play();
+        GeneralPlayer.Play();
+    }
+}
