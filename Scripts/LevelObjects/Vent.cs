0f6fd6c230c5a92258bc6e7db2dcf3e81160b54f
Katsu Rian <redkrsc@gmail.com>
2024-08-20 11:06:31 -0400

    Auto stash before merge of "main" and "MergePleaseGod"

--- Scripts/LevelObjects/Vent.cs
+++ Scripts/LevelObjects/Vent.cs
@@ -6,6 +6,6 @@ public partial class Vent : Sprite2D
 	public bool open = true;
 
 	private void _on_click_detector_on_click(Node context) {
-		RobotSpawner.Instance.CreateRobot(0, GlobalPosition);
+		RobotSpawner.Instance.CreateRobot(RobotStorage.Instance.currentSelectedPreset, GlobalPosition);
 	}
 }
