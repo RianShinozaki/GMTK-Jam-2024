bb4a9834de001b08ad65b605726563f0f8176b72
Katsu Rian <redkrsc@gmail.com>
2024-08-20 12:28:26 -0400

    Added destroyed sprites

---
+++ Nodes/LevelPieces/DestroyableStation.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class DestroyableStation : BasicPhysicsObject
+{
+	public bool Burned;
+	public bool Screwed;
+
+	Node2D station;
+
+	[Export] public bool shouldBeBurned;
+	[Export] public bool shouldBeScrewed;
+
+    public override void _Ready()
+    {
+		station = GetParent<Node2D>();
+        base._Ready();
+    }
+    public override void _Process(double delta)
+    {
+		if(Screwed && shouldBeScrewed || Burned && shouldBeBurned) {
+			station.Call("_destroy_station");
+			GD.Print("woaw");
+		}
+        base._Process(delta);
+    }
+
+}
