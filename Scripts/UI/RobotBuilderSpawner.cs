e7493854ba9ca69ee8ca5670be905a4a77e457ef
CrashCanHero <trashcunhero@gmail.com>
2024-08-20 11:01:56 -0400

    Revert builder changes

--- Scripts/UI/RobotBuilderSpawner.cs
+++ Scripts/UI/RobotBuilderSpawner.cs
@@ -1,24 +1,25 @@
 using Godot;
-using System;
+using System.Collections.Generic;
 
 public partial class RobotBuilderSpawner : Node2D
 {
 	[Export] public string AIBotPath;
-	AiBotBase currentBot;
+	Queue<AiBotBase> botQueue = new Queue<AiBotBase>();
 	public static RobotBuilderSpawner Instance;
-    public override void _Ready()
-    {
+    public override void _Ready() {
         base._Ready();
 		CreateRobot(0);
 		Instance = this;
     }
+
     public void CreateRobot(int preset) {
-		if(currentBot != null) {
-			//currentBot.QueueFree();
+		if(botQueue.Count >= 5) {
+			botQueue.Dequeue().QueueFree();
 		}
-		currentBot = (AiBotBase)GD.Load<PackedScene>(AIBotPath).Instantiate();
+		AiBotBase currentBot = (AiBotBase)GD.Load<PackedScene>(AIBotPath).Instantiate();
 		currentBot.AndroidBase = RobotStorage.Instance.robots[preset];
 		currentBot.Init();
 		AddChild(currentBot);
+		botQueue.Enqueue(currentBot);
 	}
 }
